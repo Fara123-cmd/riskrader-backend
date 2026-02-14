31c1eca767cba7d28a857588b0b3195d633c0ef3
Fara Fathima <farafathima.i2005@gmail.com>
2026-02-14 22:26:59 +0530

    Fix firebase-admin and Render port binding

--- app.py
+++ app.py
@@ -94,9 +94,9 @@ def predict():
 # -------------------------
 # RUN SERVER
 # -------------------------
+import os
+
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",   # allow all network devices
-        port=5000,
-        debug=True
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
